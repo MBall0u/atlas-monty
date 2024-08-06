2877dd5252d4e98cd6d9d0909396a1b85ba709ff
“MBall0u” <167779488+MBall0u@users.noreply.github.com>
2024-08-05 22:14:34 -0500

    Well it looks like i am only getting through this project tonight

--- main.c
+++ main.c
@@ -6,11 +6,34 @@
 */
 int main(char *file_name)
 {
-	int line_num = 0;
+	char buf[1024];
+	size_t i;
+	int (*my_func)(stack_t **, unsigned int);
+	unsigned int line_num = 0;
+	char *str, str_p;
+	int global_int;
+	stack_t *head = NULL;
 	FILE *file = fopen(file_name, "r");
 	if (file == NULL)
 	{
 		printf("Failed to open file.\n");
 		exit(EXIT_FAILURE);
+	}
+
+	while (fgets(buf, sizeof(buf), file))
+	{
+		line_num++;
+		i = strlen(buf);
+		if (i > 0 && buf[i - 1] == '\n')
+			buf[i - 1] = '\0';
+		str = strtok_r(buf, " ", &str_p);
+		my_func = get_func(str);
+		if (my_func != NULL)
+		{
+			str = strtok_r(NULL, "$", &str_p);
+			sscanf(str, "%d", &global_int);
+			if (global_int != 0)
+				my_func(&head, line_num);
+		}
 	}
 }
--- monty.h
+++ monty.h
@@ -5,6 +5,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+extern int global_int;
+
 /**
  * struct stack_s - doubly linked list representation of a stack (or queue)
  * @n: integer
